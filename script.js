0cf04912b8dbbccf73716736989e3de8ed98ede4
stylehacks77-alt <stylehacks77@gmail.com>
2026-04-12 00:46:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -51,61 +51,61 @@ function sincronizar(v) {
     let info = "";
     switch(v) {
         case "Argentina": 
-            info = "🇦🇷 Mercado Pago | CVU: 0000003100006100224001 | | Titular: Cristian Ezequiel"; 
+            info = "🇦🇷 Ualá | Nombre: César Correa | CVU: 0000184305010007732302 | Alias: cescorrea1"; 
             break;
         case "Bolivia": 
-            info = "🇧🇴 Banco yape: N° Cuenta 76677904 | QR disponible al privado"; 
+            info = "🇧🇴 Yape: N° Cuenta 52656932 | Yape QR: Código disponible en imagen"; 
             break;
         case "Brasil": 
-            info = "🇧🇷 PIX: Chave ariannyyoselinp@gmail.com| Titular: "; 
+            info = "🇧🇷 PIX: Chave 91991076791"; 
             break;
         case "Chile": 
-            info = "🇨🇱 Banco Estado (Cuenta corriente): 19843276235 | Titular: leonel Chávez | RUT: 29.061.432-5 -K"; 
+            info = "🇨🇱 Banco Estado (CuentaRUT): 23710151 | Titular: XAVIER FUENZALIDA | RUT: 23.710.151-0"; 
             break; 
         case "Colombia": 
-            info = "🇨🇴 NEQUI / Bancolombia: 3233438983 | Titular: STYLEHACKS OFICIAL"; 
+            info = "🇨🇴 NEQUI: 3233438983"; 
             break;
         case "Costa Rica": 
-            info = "🇨🇷 SINPE Móvil: 68823166 | Titular: wilber gonzales"; 
+            info = "🇨🇷 SINPE Móvil: 72805302"; 
             break;
         case "Ecuador": 
-            info = "🇪🇨 Banco Pichincha: N° Cuenta 2210169007"; 
+            info = "🇪🇨 Banco Pichincha: N° Cuenta 2207195565"; 
             break;
         case "España": 
-            info = "🇪🇸 Bizum / BBVA: 34643172469 | Titular: Roberto tejeda"; 
+            info = "🇪🇸 Bizum: 637 07 09 26 (Xiomari Moreno)"; 
             break;
         case "USA": 
-            info = "🇺🇸 Zelle: elbateresa26@gmail.com| Titular: elba Oviedo (Mínimo $20)"; 
+            info = "🇺🇸 Zelle: elbateresa26@gmail.com (Mínimo $20.00 USD)"; 
             break;
         case "Guatemala": 
-            info = "🇬🇹 Banco Industrial: N° Cuenta 4442201736| Bi Fáciles"; 
+            info = "🇬🇹 Banrural: N° Cuenta 4431164091"; 
             break;
         case "Honduras": 
-            info = "🇭🇳 banco azteca N° Cuenta 38510116351825 | Titular: Oscar Gómez"; 
+            info = "🇭🇳 Bampais: N° Cuenta 216400100524"; 
             break;
         case "Mexico": 
-            info = "🇲🇽 mercado pago/ Guardadito: N° 722969017644052486 |titular: Victor cortez| Oxxo México: 4217470291863799"; 
+            info = "🇲🇽 Albo: 721180100042683432 | Nu México (OXXO): 5101 2506 8691 9389"; 
             break;
         case "Nicaragua": 
-            info = "🇳🇮 Banpro: 10021455632147 | IBAN: NI45BANP0000000001002145563214"; 
+            info = "🇳🇮 BAC: 371674409 | IBAN: NI37BAMC0000000000371674409"; 
             break;
         case "Panama": 
-            info = "🇵🇦 Banco General: N° Cuenta 04-72-98-554321-0 (Ahorros)"; 
+            info = "🇵🇦 Consultar datos de transferencia al privado."; 
             break;
         case "Paraguay": 
-            info = "🇵🇾 Banco itau: 320553574 nombre:yeruti Mariela| cédula:4988245| alias yerutibaez25@gmail.com*; 
+            info = "🇵🇾 Itaú: 300406285 (Diego Leiva) | Billetera Personal: 0993363424"; 
             break;
         case "Peru": 
-            info = "🇵🇪 BCP: 191-98776543-0-12 | Yape / Plin: 954302258 (Xiomari Moreno)"; 
+            info = "🇵🇪 Yape / Plin: 954302258"; 
             break;
         case "Republica Dominicana": 
-            info = "🇩🇴 Banco Popular: 849314034: Manuel Martinez| Banreservas: 9606178055 | Qik: 1002559567"; 
+            info = "🇩🇴 Popular: 837147719 | BHD: 34478720012 | Qik: 1002173707"; 
             break;
         case "Uruguay": 
-            info = "🇺🇾 Prex / Mi Dinero: Consultar número de cuenta actual al WhatsApp."; 
+            info = "🇺🇾 Consultar datos locales al WhatsApp."; 
             break;
         case "Venezuela": 
-            info = "🇻🇪 Venezuela (0102) | 04128240604 | V-31.376.662 | Pago Móvil"; 
+            info = "🇻🇪 Venezuela (0102) | 31.376.662 | 0412-8240604 | Pago Móvil"; 
             break;
         default: 
             info = "Selecciona un país para ver los datos bancarios.";
